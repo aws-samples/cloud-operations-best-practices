70247e566a010f06ea6c6fbe490fca502ef5006e
Isaiah Salinas <ezsali@amazon.com>
2025-01-29 15:20:13 -0500

    updated docusaurus config

--- cloud-operations-best-practices/docusaurus.config.js
+++ cloud-operations-best-practices/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   title: 'AWS Cloud Operations Best Practices',
 
   // Set the production url of your site here
-  url: 'https://aws-samples.github.io/cloud-operations-best-practices',
+  url: 'https://aws-samples.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cloud-operations-best-practices/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
