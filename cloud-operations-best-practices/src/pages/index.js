ca85177251ca339190df0a08309adf00532da22a
Isaiah Salinas <ezsali@amazon.com>
2025-01-30 15:33:36 -0500

    Added Metrics Configuration

--- cloud-operations-best-practices/src/pages/index.js
+++ cloud-operations-best-practices/src/pages/index.js
@@ -7,8 +7,33 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+import { AwsRum } from 'aws-rum-web';
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  try {
+    const config = {
+      sessionSampleRate: 1,
+      identityPoolId: "us-east-2:27bab94f-d1e8-4816-a2b1-4b744240f6e0",
+      endpoint: "https://dataplane.rum.us-east-2.amazonaws.com",
+      telemetries: ["performance","errors","http"],
+      allowCookies: false,
+      enableXRay: false
+    };
+
+    const APPLICATION_ID = '7fd9b3a4-6d10-450c-8ab6-a645ce8539c6';
+    const APPLICATION_VERSION = '1.0.0';
+    const APPLICATION_REGION = 'us-east-2';
+
+    const awsRum = new AwsRum(
+      APPLICATION_ID,
+      APPLICATION_VERSION,
+      APPLICATION_REGION,
+      config
+    );
+  } catch (error) {
+    // Ignore errors thrown during CloudWatch RUM web client initialization
+  }
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
